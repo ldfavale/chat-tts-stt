17e5a11e191e39632d0dc28c44f04820a8b86efa
Leonardo Favale <ldfavale@gmail.com>
2025-02-11 17:18:20 -0300

    Fix Typescript Warnigs

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,70 +1,76 @@
 import { PauseIcon, PlayIcon } from "@heroicons/react/20/solid";
 import { useState, useRef, useEffect } from "react";
 
+interface Message {
+    text: string;
+    role: "user" | "system";
+  }
+
 const Chat = () => {
-  const [messages, setMessages] = useState([]); // Almacena los mensajes
-  const [inputText, setInputText] = useState(""); // Almacena el texto del input
-  const chatContainerRef = useRef(null); // Referencia al contenedor del chat
-  const audioElement = useRef(null);
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [inputText, setInputText] = useState(""); 
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const chatContainerRef = useRef<HTMLDivElement>(null);
   
-  // Efecto para hacer scroll automático al final del chat
   useEffect(() => {
     if (chatContainerRef.current) {
       chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
     }
   }, [messages]);
 
-  // Función para manejar el envío de mensajes
+  
   const handleSendMessage = async () => {
-    if (!inputText.trim()) return; // Evitar mensajes vacíos
+    if (!inputText.trim()) return; 
 
-    // Agregar el mensaje del usuario al chat
     setMessages((prevMessages) => [
       ...prevMessages,
       { text: inputText, role: "user" },
     ]);
 
     setIsLoading(true);
-    // Limpiar el input
     setInputText("");
+
     try {
         const chatResponse = await handleSendPrompt();
+        const audioResponse = await fetchTTS(chatResponse)
+        const audioBlob = await audioResponse.blob();
+        const audioUrl = URL.createObjectURL(audioBlob);
+
+        if(audioRef && audioRef.current ){
+          audioRef.current.src = audioUrl;
+          audioRef.current.play();
+          setIsPlaying(true)
+        }
+
         setMessages((prevMessages) => [
-            ...prevMessages.filter((msg) => msg.text !== "Procesando..."), // Eliminar el mensaje de "Procesando..."
+            ...prevMessages, 
             { text: chatResponse, role: "system" },
-            ]);
+        ]);
+
     } catch (error) {
         console.error("Error:", error);
         setMessages((prevMessages) => [
             ...prevMessages,
-            { text: "Error al obtener la respuesta", sender: "bot" },
+            { text: "Error al obtener la respuesta", role: "system" },
           ]);
     }finally{
         setIsLoading(false);
     }
-
-    
-   
-   
-   
   };
-
-
-  
 
   const handleSendPrompt = async () => {
     try {
       const chatResponse = await fetchChatGPT(inputText);
-      const audio = await fetchTTS(chatResponse);
       return chatResponse;
     } catch (error) {
       console.error("Error fetching response:", error);
     }
   };
 
-  const fetchChatGPT = async (prompt) => {
+  const fetchChatGPT = async (prompt: string) => {
     const res = await fetch("http://localhost:3001/api/chatgpt", {
       method: "POST",
       headers: {
@@ -75,35 +81,26 @@ const Chat = () => {
     const data = await res.json();
     return data;
   };
-  const fetchTTS = async (message) => {
-    const response = await fetch("http://localhost:3001/api/tts", {
+
+  const fetchTTS = async (message: string) => {
+    return await fetch("http://localhost:3001/api/tts", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ message }),
-    });
-    
-    const audioBlob = await response.blob();
-    const audioUrl = URL.createObjectURL(audioBlob);
-    if(audioElement && audioElement.current ){
-      audioElement.current.src = audioUrl;
-      audioElement.current.play();
-      setIsPlaying(true)
-    }
-    
+    });    
   };
 
   const handlePlayPause = () => {
-    if (!audioElement.current) return;
+    if (!audioRef.current) return;
 
     if (isPlaying) {
-      audioElement.current.pause();
+      audioRef.current.pause();
       setIsPlaying(false);
     }else{
-      audioElement.current.play();
+      audioRef.current.play();
       setIsPlaying(true);
-
     }
   }
   
@@ -126,12 +123,12 @@ const Chat = () => {
           overflowY: "auto",
           marginBottom: "16px",
           display: "flex",
-          flexDirection: "column-reverse", // Mostrar los mensajes más recientes arriba
+          flexDirection: "column-reverse", // Mostrar los mensajes más recientes abajo
         }}
       >
         {messages
           .slice() // Crear una copia del array para no modificar el original
-          .reverse() // Invertir el orden para mostrar los mensajes más recientes arriba
+          .reverse() // Invertir el orden para mostrar los mensajes más recientes abajo
           .map((message, index) => (
             <div
               key={index}
@@ -158,7 +155,7 @@ const Chat = () => {
             }}
           >
             <img
-              src="https://i.gifer.com/ZZ5H.gif" // URL de un gif de carga
+              src="https://i.gifer.com/ZZ5H.gif"
               alt="Loading..."
               className={"w-10 h-10 p-2"}
             />
@@ -198,7 +195,7 @@ const Chat = () => {
         >
           Enviar
         </button>
-        <audio id="audioPlayer" ref={audioElement} ></audio>
+        <audio id="audioPlayer" ref={audioRef} ></audio>
        <button onClick={() => handlePlayPause()} className="p-4  bg-blue-400 rounded-full" >
        { isPlaying ? 
             <PauseIcon className="h-6 w-6 text-white" /> :
