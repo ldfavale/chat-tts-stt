4715ab2b7fabb497c28b70b1fbc1cf03f7368b82
Leonardo Favale <ldfavale@gmail.com>
2025-02-13 17:02:24 -0300

    Change BACKEND_BASE_URL constant

--- constants.tsx
+++ constants.tsx
@@ -3,6 +3,6 @@ export const POST = 'POST'
 export const PUT = 'PUT'
 export const PATCH = 'PATCH'
 export const DELETE = 'DELETE'
-export const BACKEND_BASE_URL = import.meta.env.BACKEND_BASE_URL || 'http://localhost:8888/.netlify/functions'
+export const BACKEND_BASE_URL = 'https://chat-tts-stt.netlify.app/.netlify/functions'
 export const DEEPSEEK_URL = `${BACKEND_BASE_URL}/deepseek`
 export const ELEVENLABS_TTS_URL = `${BACKEND_BASE_URL}/tts`
